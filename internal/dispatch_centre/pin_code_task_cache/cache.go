611ec5604c46655455ab8eb842a6ad238c01c4b7
gang <ynwlgh@gmail.com>
2024-03-22 15:08:01 +0800

    updaet

--- internal/dispatch_centre/pin_code_task_cache/cache.go
+++ internal/dispatch_centre/pin_code_task_cache/cache.go
@@ -31,20 +31,20 @@ type Wrap struct {
 
 func (slf *Cache) DispatchAndWaitFinish(pinCode string, item *Task.Item, timeout time.Duration) ([]byte, error) {
 
-	log.Trace().Str("pinCode", pinCode).Interface("发布任务", item).Send()
+	log.Info().Str("pinCode", pinCode).Interface("发布任务", item).Send()
 
 	val, ok := slf.claimBufChannel.LoadAndDelete(pinCode)
 	if !ok {
-		log.Trace().Str("pinCode", pinCode).Msg("no client for this pinCode waiting")
+		log.Info().Str("pinCode", pinCode).Msg("no client for this pinCode waiting")
 		return nil, fmt.Errorf("no client for this pinCode waiting")
 	}
 
 	var channel = val.(chan *Task.Item)
 
-	log.Trace().Str("pinCode", pinCode).Msg("等待任务被认领")
+	log.Info().Str("pinCode", pinCode).Msg("等待任务被认领")
 	channel <- item
 
-	log.Trace().Str("pinCode", pinCode).Msg("任务已被认领, 等待任务完成")
+	log.Info().Str("pinCode", pinCode).Msg("任务已被认领, 等待任务完成")
 	var finishChannel = make(chan Wrap)
 	slf.mFinishChannel.Store(item.Id, finishChannel)
 
@@ -52,11 +52,11 @@ func (slf *Cache) DispatchAndWaitFinish(pinCode string, item *Task.Item, timeout
 
 	select {
 	case <-t.C:
-		log.Trace().Str("pinCode", pinCode).Msg("超时")
+		log.Info().Str("pinCode", pinCode).Msg("超时")
 		slf.mFinishChannel.Delete(item.Id)
 		return nil, fmt.Errorf("timeout")
 	case wrap := <-finishChannel:
-		log.Trace().Str("pinCode", pinCode).Msg("已完成")
+		log.Info().Str("pinCode", pinCode).Msg("已完成")
 		return wrap.payload, wrap.err
 	}
 
@@ -64,7 +64,7 @@ func (slf *Cache) DispatchAndWaitFinish(pinCode string, item *Task.Item, timeout
 
 func (slf *Cache) ClaimAndWait(pinCode string) (*Task.Item, error) {
 
-	log.Trace().Str("pinCode", pinCode).Msg("认领任务")
+	log.Info().Str("pinCode", pinCode).Msg("认领任务")
 
 	var channel = make(chan *Task.Item)
 	slf.claimBufChannel.Store(pinCode, channel)
@@ -73,11 +73,11 @@ func (slf *Cache) ClaimAndWait(pinCode string) (*Task.Item, error) {
 	var t = time.NewTimer(time.Hour)
 	select {
 	case <-t.C:
-		log.Trace().Str("pinCode", pinCode).Msg("认领任务超时")
+		log.Info().Str("pinCode", pinCode).Msg("认领任务超时")
 		slf.claimBufChannel.Delete(pinCode)
 		return nil, fmt.Errorf("timeout")
 	case taskItem := <-channel:
-		log.Trace().Str("pinCode", pinCode).Msg("已认领到一个任务")
+		log.Info().Str("pinCode", pinCode).Msg("已认领到一个任务")
 		return taskItem, nil
 	}
 
@@ -85,19 +85,19 @@ func (slf *Cache) ClaimAndWait(pinCode string) (*Task.Item, error) {
 
 func (slf *Cache) Finish(id string, payload []byte, err error) error {
 
-	log.Trace().Str("taskId", id).Msg("完成任务")
+	log.Info().Str("taskId", id).Msg("完成任务")
 
 	if val, ok := slf.mFinishChannel.LoadAndDelete(id); ok {
 
 		var channel = val.(chan Wrap)
 
-		log.Trace().Str("taskId", id).Msg("发送给发布方")
+		log.Info().Str("taskId", id).Msg("发送给发布方")
 		channel <- Wrap{
 			err:     err,
 			payload: payload,
 		}
 
-		log.Trace().Str("taskId", id).Msg("对方已接收")
+		log.Info().Str("taskId", id).Msg("对方已接收")
 
 		return nil
 	} else {
--- internal/dispatch_centre/task_type_cache/cache.go
+++ internal/dispatch_centre/task_type_cache/cache.go
@@ -29,11 +29,12 @@ type Wrap struct {
 
 func (slf *Cache) DispatchAndWaitFinish(item *Task.Item, timeout time.Duration) ([]byte, error) {
 
-	log.Trace().Interface("dispatch", item).Send()
+	log.Info().Str("taskId", item.Id).Msg("发布任务并等待认领")
 
 	//send to claim wait
 	slf.claimBufChannel <- item
 
+	log.Info().Str("tasId", item.Id).Msg("等待结束")
 	//wait for finish
 	var finishChannel = make(chan Wrap)
 	slf.mFinishChannel.Store(item.Id, finishChannel)
@@ -42,8 +43,11 @@ func (slf *Cache) DispatchAndWaitFinish(item *Task.Item, timeout time.Duration) 
 
 	select {
 	case <-t.C:
+		log.Info().Str("tasId", item.Id).Msg("等待超时")
+		slf.mFinishChannel.Delete(item.Id)
 		return nil, fmt.Errorf("timeout")
 	case wrap := <-finishChannel:
+		log.Info().Str("tasId", item.Id).Msg("收到任务结果")
 		return wrap.payload, wrap.err
 	}
 
@@ -51,14 +55,14 @@ func (slf *Cache) DispatchAndWaitFinish(item *Task.Item, timeout time.Duration) 
 
 func (slf *Cache) ClaimAndWait(ctx context.Context) (*Task.Item, error) {
 
-	log.Trace().Msg("ClaimAndWait")
+	log.Info().Msg("认领任务..")
 
 	select {
 	case taskItem := <-slf.claimBufChannel:
-		//wait for a task come
+		log.Info().Msg("认领到了一个任务")
 		return taskItem, nil
 	case <-ctx.Done():
-		log.Info().Msg("http request cancelled")
+		log.Info().Msg("认领任务 http request 已取消")
 		return nil, fmt.Errorf("http request cancelled")
 	}
 
@@ -66,11 +70,11 @@ func (slf *Cache) ClaimAndWait(ctx context.Context) (*Task.Item, error) {
 
 func (slf *Cache) Finish(id string, payload []byte, err error) error {
 
+	log.Info().Str("taskId", id).Msg("提交任务结果")
+
 	if payload != nil && err != nil {
 		return fmt.Errorf("logic err, pass payload or err")
 	}
-
-	log.Trace().Str("finish", id).Send()
 
 	if val, ok := slf.mFinishChannel.LoadAndDelete(id); ok {
 
@@ -81,8 +85,11 @@ func (slf *Cache) Finish(id string, payload []byte, err error) error {
 			payload: payload,
 		}
 
+		log.Info().Str("taskId", id).Msg("已提交结果给任务发布方")
+
 		return nil
 	} else {
+		log.Info().Str("taskId", id).Msg("can no find this task id in dispatching, 可能已经超时? 发布都取消了?")
 		return fmt.Errorf("can no find this task id in dispatching")
 	}
 
