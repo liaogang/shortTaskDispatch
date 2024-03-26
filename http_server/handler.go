21e566038a46649d06cefae0dcfabfc11d47e4ca
gang <ynwlgh@gmail.com>
2024-03-26 16:46:26 +0800

    update

--- http_server/handler.go
+++ http_server/handler.go
@@ -9,29 +9,25 @@ import (
 	"root/extend/shortid"
 	"root/internal/dispatch_centre"
 	"strconv"
+	"sync"
 	"time"
 )
 
 var NameToImpl = make(map[string]dispatch_centre.DispatchImpl)
-var taskIdToImpl = make(map[string]dispatch_centre.DispatchImpl)
+var taskIdToImpl sync.Map //=  make(map[string]dispatch_centre.DispatchImpl)
 
-//const (
-//	TaskTypeQQRegisterCaptcha = "qqRegisterCaptcha"
-//	TaskTypeQQLoginCaptcha    = "qqLoginCaptcha"
-//	TaskTypeQQRegisterSms     = "qqRegisterCheckOrSendSms"
-//)
-//
-//var (
-//	QQRegisterCaptcha = task_type_cache.NewCache()
-//	QQLoginCaptcha    = task_type_cache.NewCache()
-//	QQRegisterSms     = pin_code_task_cache.NewCache()
-//)
-//
-//const (
-//	PrefixQQRegisterCaptcha = "cap0"
-//	PrefixQQLoginCaptcha    = "cap1"
-//	PrefixQQRegisterSms     = "sms0"
-//)
+func genUniqueTaskId() string {
+
+	for {
+		var id = shortid.New()
+		if _, has := taskIdToImpl.Load(id); has {
+			//already have, skip
+		} else {
+			return id
+		}
+	}
+
+}
 
 func publishTask(ctx *gin.Context) error {
 
@@ -59,7 +55,9 @@ func publishTask(ctx *gin.Context) error {
 
 	pinCode := ctx.Query("pinCode")
 
-	var taskId = shortid.New()
+	var taskId = genUniqueTaskId()
+
+	taskIdToImpl.Store(taskId, impl)
 
 	var item = &Task.Item{
 		Id:   taskId,
@@ -100,7 +98,6 @@ func claimTask(ctx *gin.Context) error {
 func finishTask(ctx *gin.Context) error {
 
 	taskId := ctx.Query("taskId")
-
 	errParam := ctx.Query("bodyIsError")
 
 	body, err := io.ReadAll(ctx.Request.Body)
@@ -110,10 +107,12 @@ func finishTask(ctx *gin.Context) error {
 
 	ctx.Request.Body.Close()
 
-	impl, ok := taskIdToImpl[taskId]
+	val, ok := taskIdToImpl.LoadAndDelete(taskId)
 	if !ok {
 		return fmt.Errorf("no this task type")
 	}
+
+	impl := val.(dispatch_centre.DispatchImpl)
 
 	if errParam == "1" {
 		return impl.Finish(taskId, nil, errors.New(string(body)))
